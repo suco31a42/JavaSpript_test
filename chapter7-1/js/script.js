6c29cf8b2217f71fd790246864bdf82cc31a4692
suco31a42 <132245813+suco31a42@users.noreply.github.com>
2023-09-24 14:42:54 +0000

    [update]chapter7-1 script.js

--- chapter7-1/js/script.js
+++ chapter7-1/js/script.js
@@ -45,13 +45,13 @@ window.addEventListener('load', () => {
         opacity: 1,
         offset: .8 //80%
       },
-     
+
       {
         opacity: 0,
         offset: 1 //100%
       }
     ],
-  
+
     {
       duration: 1200,
       easing: 'ease',
@@ -67,6 +67,12 @@ window.addEventListener('load', () => {
 const mainImage = document.querySelector('.gallery-image img');
 const thumbImages = document.querySelectorAll('.gallery-thumbnails')
 
+// for(let i = 0; i < thumbImages.length; i++) {
+//   thumbImages[i].addEventListener('mouseover',(event) => {
+//     // console.log(event.target.src);
+//     mainImage.src = event.target.src;
+//     mainImage.animate({opacity: [0, 1]}, 500)//フラッシュアニメ
+//   });
 thumbImages.forEach((thumbImages) => {
   thumbImages.addEventListener('mouseover', (event) => {
     mainImage.src = event.target.src;
