ca39d0725d1ab79d1f1591dbadfe0fdfb2a1d310
suco31a42 <132245813+suco31a42@users.noreply.github.com>
2023-09-27 13:26:20 +0000

    [update]chapter7-11

--- chapter7-1/js/script.js
+++ chapter7-1/js/script.js
@@ -80,3 +80,44 @@ thumbImages.forEach((thumbImages) => {
   });
 });
 
+/* スライドメニュー
+==================================================================*/
+const menuOpen = document.querySelector('#menu-open');
+const menuClose = document.querySelector('#menu-close');
+const menuPanel = document.querySelector('#menu-panel');
+const menuItems = document.querySelectorAll('#menu-panel li')
+const menuOptions = {
+  duration: 1400,
+  easing: 'ease',
+  fill: 'forwards',
+};
+
+// メニューを開く
+menuOpen.addEventListener('click', () => {
+  // console.log('メニューを開く');
+  menuPanel.animate({translate: ['100vw', 0]}, menuOptions);
+  // リンクを１つずつ順に表示
+  menuItems.forEach((menuItem, index) => {
+    // console.log(`${index}番目`);
+    menuItem.animate(
+      {
+        opacity: [0, 1],
+        translate: ['2rem', 0],
+      },
+      {
+        duration: 2400,
+        delay: 300 * index,
+        easing: 'ease',
+        fill: 'forwards',
+      }
+    );
+  });
+});
+
+// メニューを閉じる
+menuClose.addEventListener('click', () => {
+  menuPanel.animate({translate: [0, '100vw']}, menuOptions);
+  menuItems.forEach((menuItem) => {
+    menuItem.animate({opacity: [1, 0]}, menuOptions);
+  });
+});
