31c8c8e31d6a4d4cef76a517eed907403482e696
suco31a42 <132245813+suco31a42@users.noreply.github.com>
2023-09-29 11:16:20 +0000

    [update]chapter7-12

--- chapter7-1/js/script.js
+++ chapter7-1/js/script.js
@@ -121,3 +121,37 @@ menuClose.addEventListener('click', () => {
     menuItem.animate({opacity: [1, 0]}, menuOptions);
   });
 });
+
+/*
+スクロールで要素を表示
+================================================ */
+// 監視対象が範囲内に現れたら実行する動作
+const animateFade = (entries, obs) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.animate(
+        {
+          opacity: [0, 1],
+          filter: ['blur(.4rem)', 'blur(0)'], 
+          translate: ['0 4rem', 0],
+        },
+        {
+          duration: 2000,
+          easing: 'ease',
+          fill: 'forwards',
+        }
+      );
+      // 一度ふわっと表示されたら監視をやめる
+      obs.unobserve(entry.target);
+    }
+  });
+};
+
+// 監視設定
+const fadeObserver = new IntersectionObserver(animateFade);
+
+// .fadeinを監視するよう指示
+const fadeElements = document.querySelectorAll('.fadein');
+fadeElements.forEach((fadeElement) => {
+  fadeObserver.observe(fadeElement);
+});
